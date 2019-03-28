29674dd8a335ce5bf9a192e74af27fb4e92b039d
David Albert <garkolym@googlemail.com>
2019-03-28 21:34:20 +0100

    feature: create login component

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+/* Hier werden die Komponenten geladen */
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+/* Hier werden PrimeNG Module geladen */
+import { ButtonModule } from 'primeng/button';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
