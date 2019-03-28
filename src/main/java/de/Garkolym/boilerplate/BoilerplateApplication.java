97845232344304b109bd26165b9f32cd13ae9060
David Albert <garkolym@googlemail.com>
2019-03-28 21:10:10 +0100

    Initial Commit

---
+++ src/main/java/de/Garkolym/boilerplate/BoilerplateApplication.java
@@ -0,0 +1,13 @@
+package de.Garkolym.boilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoilerplateApplication.class, args);
+	}
+
+}
