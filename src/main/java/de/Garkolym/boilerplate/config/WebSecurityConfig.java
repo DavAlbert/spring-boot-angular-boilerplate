480855d8feb083c3be49fb65b9f6169fe71fbe46
David Albert <garkolym@googlemail.com>
2019-04-06 01:04:45 +0200

    feature: Add JWT System for the websecurityconfig

---
+++ src/main/java/de/Garkolym/boilerplate/config/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package de.Garkolym.boilerplate.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    // TODO: Coming Soon ... I need to sleep ... zzzzZZ #GarkoArmy
+}
