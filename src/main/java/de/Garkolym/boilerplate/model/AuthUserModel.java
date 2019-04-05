15a57696538039354b0cc1287a6fb25f8a827e15
David Albert <garkolym@googlemail.com>
2019-04-06 00:13:06 +0200

    feature: implement user with rights to the models

---
+++ src/main/java/de/Garkolym/boilerplate/model/AuthUserModel.java
@@ -0,0 +1,101 @@
+package de.Garkolym.boilerplate.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+public class AuthUserModel extends AbstractEntity implements UserDetails, Serializable {
+
+    @Column(unique = true)
+    private String username;
+    @JsonIgnore
+    private String password;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    private boolean enabled = true;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Authority> authorities;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/de/Garkolym/boilerplate/model/UserRole.java
@@ -0,0 +1,5 @@
+package de.Garkolym.boilerplate.model;
+
+public enum UserRole {
+    ROLE_USER, ROLE_ADMIN
+}
