c4e3f93512a832df59ddde19a984fc210e4ee9fc
David Albert <garkolym@googlemail.com>
2019-04-07 00:51:29 +0200

    feature: implements loggedout checker in jwt interceptor

--- angular-client/src/app/interceptors/jwt.interceptor.ts
+++ angular-client/src/app/interceptors/jwt.interceptor.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { UserService } from '../services/user.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
+
+    constructor(private userServiec: UserService) { }
+
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         let accessToken = localStorage.getItem('Access-Token');
         if (accessToken != null) {
@@ -13,6 +18,12 @@ export class JwtInterceptor implements HttpInterceptor {
                 }
             });
         }
-        return next.handle(request);
+        return next.handle(request).pipe(catchError(err => {
+            if (err.status === 401) {
+                this.userServiec.setLoggedIn(false);
+            }
+            const error = err.error.message || err.statusText;
+            return throwError(error);
+        }));
     }
 }
